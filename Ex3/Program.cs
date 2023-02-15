6e9b0dc97f43057ac2020f373d3f0ca92fa8bc36
Sergey <sivak.93@bk.ru>
2023-02-15 14:23:07 +0500

    Add Ex3

---
+++ Ex3/Program.cs
@@ -0,0 +1,44 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+bool NameDay(int WorkDay)
+{
+    if (WorkDay > 5 )
+    {
+        return true;
+    }
+    return false;
+}
+
+bool NoWeekDay(int number)
+{
+    if (number > 0 && number <=7)
+    {
+        return true;
+    }
+    System.Console.WriteLine("Такого дня недели еще не придумали");
+    return false;
+}
+
+int WorkDay = Prompt("Введите день недели : ");
+if (NoWeekDay(WorkDay))
+{
+    if (NameDay(WorkDay))
+    {
+        System.Console.WriteLine("Выходной день");
+    }
+    else
+    {
+        System.Console.WriteLine("Будний день");
+    }
+} 
