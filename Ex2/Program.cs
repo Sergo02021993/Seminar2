a4b91064a9bb2c9e066d6bd9f582ac4c6a5e738f
Sergey <sivak.93@bk.ru>
2023-02-15 13:54:51 +0500

    Add Ex1 & Ex2

---
+++ Ex2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int NeedThirdNum(int number)
+{
+    while(number > 999)
+    {
+        number /= 10;
+    }
+    return number % 10;
+}
+
+bool ValidNum(int number)
+{
+    if (number < 100 )
+    {
+        System.Console.WriteLine("Нет третьей цифры");
+        return false;
+    }
+    return true;
+}
+
+int number = Prompt("Введите число : ");
+if (ValidNum(number))
+{
+    System.Console.WriteLine(NeedThirdNum(number));
+}
